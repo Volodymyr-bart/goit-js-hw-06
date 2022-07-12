b952d71d215bff285e7285f07e1063fd9f4b56db
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-07-12 18:55:36 +0300

    finish task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,10 +4,45 @@
 1) Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 2) Для кожного элемента li.item у спику ul#categories, знайде 
 і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+console.log(`Category: ${textTitleElementArray[i].textContent}`);
+console.log(`Elements: ${}`);
 */
 const countElementItem = document.querySelectorAll('#categories .item')
-console.log(countElementItem.length)
+// console.log(countElementItem);
+console.log(`Number of categories: ${countElementItem.length}`);
 
-const textTitleElement = document.querySelectorAll('h2')
+const textTitleElementArray = document.querySelectorAll('.item h2');
+// console.log(textTitleElementArray);
+const listUl = document.querySelectorAll('.item ul')
+// console.log(listUl);
 
-console.log(textTitleElement);
+
+for (let i = 0; i < countElementItem.length; i+=1) {    
+    console.log(`Category: ${textTitleElementArray[i].textContent}`);    
+    const itemLi = listUl[i].children
+    console.log(`Elements: ${itemLi.length}`);
+    
+}
+
+
+
+
+// ======================================================================
+// for (let i = 0; i < countElementItem.length; i+=1) {    
+//     console.log(`Category: ${textTitleElementArray[i].textContent}`);    
+// }
+
+// for (let i = 0; i < listUl.length; i+=1) {
+//     console.log(listUl[i]);
+//     const itemLi = listUl[i].querySelectorAll('li')
+//     console.log(itemLi.length);
+    
+// }
+// console.log('Other metod');
+// for (let i = 0; i < listUl.length; i+=1) {
+//     console.log(listUl[i]);
+//     const itemLi = listUl[i].children
+//     console.log(itemLi.length);
+    
+// }
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,9 @@
+/*
+1) Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+2) Додасть назву інгредієнта як його текстовий вміст.
+3) Додасть елементу клас item.
+4) Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+*/
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +12,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const elementUl = document.querySelector('#ingredients');
+let elementLi = document.createElement("li");
+
+
+console.log(elementUl);
+
+
+// for (let i = 0; i < ingredients.length; i+=1) {
+  
+//   elementLi.textContent = 'lkjl'
+//   elementUl.append(elementLi[i].textContent = ingredients[i]);
+  
+// }
+ 
