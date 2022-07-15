9bb14c98cc2013157ef0a04d150c53b79674a8f6
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-07-15 20:49:42 +0300

    finish 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -15,6 +15,58 @@
 Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
 Під час відправлення форми сторінка не повинна перезавантажуватися.
 Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт,
+де ім'я поля буде ім'ям властивості,
+а значення поля - значенням властивості.
+Для доступу до елементів форми використовуй властивість elements.
 Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 */
+
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+
+
+// console.dir(form);
+
+function onFormSubmit(event) {
+  // забирає оновлення після натискання кнопки
+  event.preventDefault();  
+  const formElements = event.currentTarget.elements; 
+  // const email = formElements.email.value;
+  // const password = formElements.password.value;
+  const { elements: { email, password } } = event.currentTarget;
+
+  if (email.value.length === 0 && password.value.length === 0) {
+    return alert(`Попередженням про те, що всі поля повинні бути заповнені!`)
+  }
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+ 
+  // if (formElements.email.value.length !== 0 && formElements.password.value.length !== 0) {
+  //   console.log("valid");
+  //   // const formData = {
+  //   //   email,
+  //   //   password,
+  //   // }
+  //   // console.log(formData);
+  // }
+  // else {
+  //   console.log(" попередженням про те, що всі поля повинні бути заповнені.");
+  // }
+  
+  event.currentTarget.reset();
+};
+// ============================================================================================
+  // const formData = {
+  //   email,
+  //   password,
+  // }
+
+  // const formData = new FormData(event.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log(name);
+  //   console.log(value);
+  // });
+
